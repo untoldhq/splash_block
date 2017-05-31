9fa742aa0e23875bdd3a9d510b80bde27ebcc05f
Mike Purvis <mikedotexe@gmail.com>
2017-05-31 14:47:20 -0700

    add block title if not empty in bootstrap modal template

--- templates/block--splash-bootstrap.tpl.php
+++ templates/block--splash-bootstrap.tpl.php
@@ -4,8 +4,13 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <?php if (!empty($elements['#block']->title)): ?>
+            <h4 class="modal-title"><?php print $elements['#block']->title; ?></h4>
+          <?php endif; ?>
         </div>
-        <?php echo $elements['#markup']; ?>
+        <div class="modal-body">
+          <?php print $elements['#markup']; ?>
+        </div>
       </div>
     </div>
   </div>
